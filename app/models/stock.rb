15aca811d0fc8102e92b287da42bfbbc9318c9a2
Felipe Torres <felipetoress@gmail.com>
2022-02-09 17:49:00 -0300

    feat: encriptando a publishable_token

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,8 @@
 class Stock < ApplicationRecord
-  def self.new_lookup(ticker_simbol)
-    client = IEX::Api::Client.new(
-      publishable_token: '',
-      endpoint: 'https://sandbox.iexapis.com/v1'
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new( publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                    endpoint: 'https://sandbox.iexapis.com/v1'
     )
+    client.price(ticker_symbol)
   end
 end
